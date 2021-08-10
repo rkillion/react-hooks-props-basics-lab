d10e8688c11da45e309eccb78e51062ff4e2d205
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-08-10 15:41:49 -0400

    commit tree b27eb797171407bd9c9f657d11a7c45c785c4ead

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+function Links({github,linkedin}) {
+    return (
+        <>
+        <h3>Links</h3>
+        <a href={github}>{github}</a>
+        <a href={linkedin}>{linkedin}</a>
+        </>
+    )
+}
+
+export default Links;
